2e62cb96bb0ec85af5ad943385d0e8afc5c55fc1
abbdelmourhit <abdohafian@gmail.com>
2017-10-10 19:29:08 +0100

    web.php

--- routes/web.php
+++ routes/web.php
@@ -19,75 +19,15 @@ Auth::routes();
 
 Route::get('/index', 'IndexController@index')->name('index');
 
-Route::get('/AjouterProfil', function(){
-    return view('AjouterProfil');
-});
+Route::resource('profs','ProfController');
+Route::resource('sgs','SgController');
+Route::resource('secretaires','SecretaireController');
+Route::resource('eleves','EleveController');
+Route::resource('parents','ParentController');
 
-Route::get('/ListeProfil', function(){
-    return view('ListeProfil');
-});
-
-Route::get('/AjouterEleve', function(){
-    return view('AjouterEleve');
-});
-
-Route::get('/ListeEleve', function(){
-    return view('ListeEleve');
-});
-
-Route::get('/ListeParent', function(){
-    return view('ListeParent');
-});
-
-Route::get('/AffectationClasse', function(){
-    return view('AffectationClasse');
-});
-
-Route::get('/ListeMatiere', function(){
-    return view('ListeMatiere');
-});
-
-Route::get('/ListeClasse', function(){
-    return view('ListeClasse');
-});
-
-Route::get('/ListeSalle', function(){
-    return view('ListeSalle');
-});
-
-Route::get('/ControleExamen', function(){
-    return view('ControleExamen');
-});
-
-Route::get('/ListeClassEmploi', function(){
-    return view('ListeClassEmploi');
-});
-
-Route::get('/ListeProfEmploi', function(){
-    return view('ListeProfEmploi');
-});
-
-Route::get('/ListeVehicule', function(){
-    return view('ListeVehicule');
-});
-
-Route::get('/ListeChauffeur', function(){
-    return view('ListeChauffeur');
-});
-
-Route::get('/AffectationChauffeur', function(){
-    return view('AffectationChauffeur');
-});
-
-Route::get('/AffectationEleve', function(){
-    return view('AffectationEleve');
-});
-
-Route::get('/ListeActivite', function(){
-    return view('ListeActivite');
-});
-
-Route::get('/ListeLivre', function(){
-    return view('ListeLivre');
-});
+Route::resource('classes','ClasseController');
+Route::resource('matieres','MatiereController');
+Route::resource('salles','SalleController');
+Route::resource('controles','ControleController');
+Route::resource('examens','ExamenController');
 
